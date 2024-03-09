ccb15382752289d7a340bca40b128341633213f1
Shreyas Jadhav <126581827+Shreejgithub@users.noreply.github.com>
2024-03-09 10:28:00 +0530

    signin/signup form && Login/Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-purple-800">
-      Namaste Everyone, lets build Netflix GPT
-    </div>
+    <Body/>
   );
 }
 
